f8e0ae03f9ee32e6e3172b6b476ec37512cc8893
AriesJ12 <ajtagle12@gmail.com>
2023-09-05 01:22:12 +0800

    initialize playround, and caseInsen function

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function getComputerChoice()
 {
-    const min = 0;
-    const max = 3;
+    //set min and maximum(max refers to the number of possible values -- 0 , 1 , 2 ->3 possible)
+    const MIN = 0;
+    const MAX = 3;
+
+    //randomize it(got it from net)
     let numericalChoice = Math.floor(Math.random() * (max - min) ) + min;
     let choice;
+
+    //get the equivalent number -> string
     switch(numericalChoice) 
     {
         case 0:
@@ -15,6 +20,36 @@ function getComputerChoice()
         case 2:
           choice = "Scissors";
     }
-
     return choice;
+}
+
+function playRound(playerSelection = prompt("Make your choice"), computerSelection = getComputerChoice())
+{
+    const ROCK = "rock";
+    const PAPER = "paper";
+    const SCISSORS = "scissor";
+
+    if (caseInsensitiveCompare(playerSelection, ROCK))
+    {
+
+    }
+    else if (caseInsensitiveCompare(playerSelection, PAPER))
+    {
+
+    }
+    else if (caseInsensitiveCompare(playerSelection, SCISSORS))
+    {
+
+    }
+    else
+    {
+
+    }
+}
+
+// copy paste from net(uses localCompare accent)
+function caseInsensitiveCompare(a, b) {
+    return typeof a === 'string' && typeof b === 'string'
+        ? a.localeCompare(b, undefined, { sensitivity: 'accent' }) === 0
+        : a === b;
 }
